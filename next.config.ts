cc851dd41de4bbf96bd185a2d19ab474727c5089
Evans Elabo <evans.elabo@amalitech.com>
2025-04-26 17:54:48 +0000

    feat(app): update layout and config for dynamic URLs and add image support

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
