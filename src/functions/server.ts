f1442f4e21e8705d0c8b5e282d778ae14af81d97
Evans Elabo <evans.elabo@amalitech.com>
2025-05-18 22:04:20 +0000

    feat(server): implement SERVER_FUNCTIONS for event API interactions

---
+++ src/functions/server.ts
@@ -0,0 +1,12 @@
+import { authAxios } from "@/lib/auth-axios";
+
+export const SERVER_FUNCTIONS = {
+  getEvents: async () => {
+    const res = await authAxios.get("/events");
+    return res.data;
+  },
+  getEvent: async (id: string) => {
+    const res = await authAxios.get(`/events/${id}`);
+    return res.data;
+  },
+};
