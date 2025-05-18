0495f4f51e2388f185213c974b288f535125869e
Evans Elabo <evans.elabo@amalitech.com>
2025-05-18 22:29:57 +0000

    refactor(events_page): enhance metadata for improved SEO and user engagement

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -3,9 +3,42 @@ import { Metadata } from "next";
 import Events from "./_components/events";
 import BackButton from "@/components/back";
 import EventsSkeleton from "./_components/event_skeleton";
+
 export const metadata: Metadata = {
-  title: "Events",
-  description: "Find all ongoing events and vote for preferred nominee",
+  title: "Events | JED - Vote for Your Favorite Nominees",
+  description:
+    "Discover and participate in exciting voting events. Support your favorite nominees across various categories. Join JED's community-driven voting platform.",
+  keywords:
+    "voting events, nominee voting, event categories, JED voting platform, online voting, event participation",
+  openGraph: {
+    title: "Events | JED - Vote for Your Favorite Nominees",
+    description:
+      "Discover and participate in exciting voting events. Support your favorite nominees across various categories.",
+    type: "website",
+    url: "https://jed-event.com/events",
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_LIVE_URL}/src/app/opengraph-image.png`,
+        width: 1200,
+        height: 630,
+        alt: "JED Events Platform",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Events | JED - Vote for Your Favorite Nominees",
+    description:
+      "Discover and participate in exciting voting events. Support your favorite nominees across various categories.",
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_LIVE_URL}/src/app/opengraph-image.png`,
+        width: 1200,
+        height: 630,
+        alt: "JED Events Platform",
+      },
+    ],
+  },
 };
 
 export default async function EventsPage() {
