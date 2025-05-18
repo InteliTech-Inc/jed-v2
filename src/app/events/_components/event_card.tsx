47e97b53fe5724fa9bbfd167b7759162902093d1
Evans Elabo <evans.elabo@amalitech.com>
2025-05-18 22:04:53 +0000

    refactor(event_card): update event prop type to Readonly and fix image source reference

--- src/app/events/_components/event_card.tsx
+++ src/app/events/_components/event_card.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { IconLivePhoto, IconEye } from "@tabler/icons-react";
 
-export default function EventCard({ event }: { event: Event }) {
+export default function EventCard({ event }: Readonly<{ event: Event }>) {
   return (
     <div className="group transition-all duration-300">
       <Link href={`/events/${event.id}`} className="block">
@@ -14,14 +14,16 @@ export default function EventCard({ event }: { event: Event }) {
             </p>
           </div>
           <Image
-            src={event.image}
+            src={event.img_url}
             alt={event.name}
             width={500}
             height={500}
             className="h-full absolute top-0 left-0 w-full object-cover scale-105 group-hover:scale-100 transition-all duration-500"
           />
           <div className="absolute bottom-0 h-1/5 left-0 w-full p-3 bg-gradient-to-t from-black via-black/70 to-transparent">
-            <p className=" group-hover:-translate-y-4 translate-y-0 transition-all duration-300 absolute bottom-2 text-white font-semibold">{event.name}</p>
+            <p className=" group-hover:-translate-y-4 translate-y-0 transition-all duration-300 absolute bottom-2 text-white font-semibold">
+              {event.name}
+            </p>
             <div className="translate-y-10 flex items-center gap-2 group-hover:translate-y-0 transition-all duration-300 absolute bottom-2 text-white font-semibold">
               <p>
                 <IconEye className="size-4 text-white/80" />
