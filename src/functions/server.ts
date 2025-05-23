3cda62ac42927fefeedc50c38bf94dbe52d3cd35
Evans Elabo <evans.elabo@amalitech.com>
2025-05-23 15:16:48 +0000

    fix(server): add voteNominee function to handle voting payload

--- src/functions/server.ts
+++ src/functions/server.ts
@@ -1,3 +1,4 @@
+import { VotingPayload } from "@/interfaces";
 import { authAxios } from "@/lib/auth-axios";
 
 export const SERVER_FUNCTIONS = {
@@ -7,6 +8,10 @@ export const SERVER_FUNCTIONS = {
   },
   getEvent: async (id: string) => {
     const res = await authAxios.get(`/events/${id}`);
+    return res.data;
+  },
+  voteNominee: async (votingPayload: VotingPayload) => {
+    const res = await authAxios.post("/voting", votingPayload);
     return res.data;
   },
 };
