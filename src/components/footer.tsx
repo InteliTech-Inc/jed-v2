63d17ce3b11e39d5791419777e8a9de9f3166ef2
Evans Elabo <evans.elabo@amalitech.com>
2025-05-24 06:13:49 +0000

    refactor(footer): improve code formatting and enhance readability of JSX elements

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -12,7 +12,9 @@ export default function Footer() {
       {/* Footer content */}
       <div className="grid text-center gap-8 md:grid-cols-2 lg:grid-cols-3 p-10 lg:place-content-center">
         <div className="flex flex-col">
-          <h1 className="mx-auto md:ml-0 font-semibold text-xl w-fit">Useful Links</h1>
+          <h1 className="mx-auto md:ml-0 font-semibold text-xl w-fit">
+            Useful Links
+          </h1>
           <div className="flex flex-col  md:text-left my-6 space-y-3 text-md">
             <Link href={"/"} className="hover:text-accent">
               Home
@@ -26,23 +28,34 @@ export default function Footer() {
             <Link href={"/#faq"} className="hover:text-accent">
               FAQs
             </Link>
-            <Link href={"/#faq"} className="hover:text-accent">
+            <Link href={"/topboy/chat"} className="hover:text-accent">
               Vote Now
             </Link>
           </div>
         </div>
         <div className="flex flex-col md:items-start ">
-          <h1 className="font-semibold md:text-left text-xl">Contact & Address</h1>
+          <h1 className="font-semibold md:text-left text-xl">
+            Contact & Address
+          </h1>
           <div className="flex flex-col items-center md:items-start md:text-left my-6 space-y-3 text-md">
-            <a href="mailto:info.jedvotes@gmail.com" className="hover:text-accent flex items-center gap-2">
+            <a
+              href="mailto:info.jedvotes@gmail.com"
+              className="hover:text-accent flex items-center gap-2"
+            >
               <Icon icon={"material-symbols-light:mail-outline"} />
               info.jedvotes@gmail.com
             </a>
-            <a href="tel:0599774425" className="hover:text-accent flex items-center gap-2">
+            <a
+              href="tel:0599774425"
+              className="hover:text-accent flex items-center gap-2"
+            >
               <Icon icon={"solar:phone-calling-linear"} />
               0599774425 | 0538122885
             </a>
-            <a href="http://wa.me/+233264602978" className="hover:text-accent flex items-center gap-2">
+            <a
+              href="http://wa.me/+233264602978"
+              className="hover:text-accent flex items-center gap-2"
+            >
               <Icon icon={"uil:whatsapp"} />
               Contact us on Whatsapp
             </a>
@@ -58,7 +71,9 @@ export default function Footer() {
       </div>
       <section className="border-t-secondary/30 text-white/90 p-6 flex flex-col lg:flex-row border-t px-6 mx-auto justify-between">
         <section className=" inline-flex flex-col lg:flex-row text-center items-center gap-4">
-          <p className=" mx-auto text-sm text-inherit ">&copy; {date} InteliTech Inc. All rights reserved.</p>
+          <p className=" mx-auto text-sm text-inherit ">
+            &copy; {date} InteliTech Inc. All rights reserved.
+          </p>
           <ul className="flex gap-4 text-sm">
             <li className="hover:text-accent">
               <Link href="/legal/terms">Terms of Use</Link>
@@ -70,14 +85,35 @@ export default function Footer() {
         </section>
         <section>
           <div className="flex justify-center py-6 gap-4">
-            <a href="https://www.instagram.com/jed.event?igsh=MTVxNmk3cnJlYWxuNA==" target="_blank" rel="noopener noreferrer">
-              <Icon icon={"proicons:instagram"} className="hover:text-accent size-6 transition-all duration-300 cursor-pointer hover:-translate-y-1" />
+            <a
+              href="https://www.instagram.com/jed.event?igsh=MTVxNmk3cnJlYWxuNA=="
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon
+                icon={"proicons:instagram"}
+                className="hover:text-accent size-6 transition-all duration-300 cursor-pointer hover:-translate-y-1"
+              />
             </a>
-            <Link href="https://www.linkedin.com/company/ican-code/" target="_blank" rel="noopener noreferrer">
-              <Icon icon={"jam:linkedin-square"} className="hover:text-accent size-6 transition-all duration-300 cursor-pointer hover:-translate-y-1" />
+            <Link
+              href="https://www.linkedin.com/company/ican-code/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon
+                icon={"jam:linkedin-square"}
+                className="hover:text-accent size-6 transition-all duration-300 cursor-pointer hover:-translate-y-1"
+              />
             </Link>
-            <a href="https://x.com/jed_event?t=rrgklc_dChJnFjdFtUAasA&s=09" target="_blank" rel="noopener noreferrer">
-              <Icon icon={"proicons:x-twitter"} className="hover:text-accent size-6 transition-all duration-300 cursor-pointer hover:-translate-y-1" />
+            <a
+              href="https://x.com/jed_event?t=rrgklc_dChJnFjdFtUAasA&s=09"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon
+                icon={"proicons:x-twitter"}
+                className="hover:text-accent size-6 transition-all duration-300 cursor-pointer hover:-translate-y-1"
+              />
             </a>
           </div>
         </section>
